279b3006549b04562cc842eaf5ede228dcaa947c
yilin <yilinyan1030@gmail.com>
2018-12-31 17:56:27 +0800

    shader

--- src/curves/createCurve.js
+++ src/curves/createCurve.js
@@ -7,7 +7,43 @@ var createCurve = (sides = 8, subdivs = 50, openEnded = false) => {
   const length = 1;
   const baseGeometry = new THREE.CylinderGeometry(radius, radius, length, sides, subdivs, openEnded);
 
- // baseGeometry.rotateZ(Math.PI / 2);
+  // put it horizontally
+  baseGeometry.rotateZ(Math.PI / 2);
 
-  return baseGeometry;
+  const angles = [];
+  const postions = [];
+  const vertices = baseGeometry.vertices;
+
+  baseGeometry.faces.forEach((face, i) => {
+    const {a, b, c} = face;
+    const verts = [vertices[a], vertices[b], vertices[c]];
+    // the first layer of uvs, and the i-th
+    const uvs = baseGeometry.faceVertexUvs[0][i];
+
+//    console.log("verts", verts);
+
+    verts.forEach((v, j) => {
+      // maybe need normalize ? (seems to be the same)
+      // count the radial angle
+      const angle = Math.atan2(v.z, v.y);
+      angles.push(angle);
+
+      postions.push(v.x);
+
+//      console.log("angle", angle, "v.x", v.x);
+    })
+  });
+
+  const angleArray = new Float32Array(angles);
+  const posArray = new Float32Array(postions);
+
+  console.log(angleArray);
+  console.log(posArray);
+
+  const geometry = new THREE.BufferGeometry();
+  geometry.addAttribute('position', new THREE.BufferAttribute(posArray, 1));
+  geometry.addAttribute('angle', new THREE.BufferAttribute(angleArray, 1));
+
+  baseGeometry.dispose();
+  return geometry;
 }
--- src/curves/main.js
+++ src/curves/main.js
@@ -1,18 +1,28 @@
-
 const scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.z = 5;
+camera.position.z = 10;
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 var curve = createCurve();
-var material = new THREE.MeshBasicMaterial( {color: 0xff0000} );
+// MeshBasicMaterial in THREE.js is like a toon shader (
+// good for silouhette, shadow drawing or wireframe) and and is not affected by lights.
+// https://stackoverflow.com/a/16624721/7720237
+var material = new THREE.MeshPhongMaterial( {color: 0xff1111} );
 var cylinder = new THREE.Mesh( curve, material );
 scene.add( cylinder );
 
+var light = new THREE.PointLight( 0xfefeee, 1, 100 );
+light.position.set( 4, 4, 0 );
+scene.add( light );
+
+var light = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( light );
+
 function animate() {
-	requestAnimationFrame( animate );
+  requestAnimationFrame( animate );
+  cylinder.rotateZ(0.01);
 	renderer.render( scene, camera );
 }
 animate();
